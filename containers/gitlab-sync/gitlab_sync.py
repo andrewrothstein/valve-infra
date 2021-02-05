6efe8028975487e6aa742d6ab3632c00d3d8e3fc
Charlie Turner <cturner@igalia.com>
2021-02-05 00:53:07 +0000

    Move gitlab-sync to an isolated service.

---
+++ containers/gitlab-sync/gitlab_sync.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python3
+# -*- mode: python -*-
+
+from pprint import pformat
+import gitlab
+from datetime import datetime
+import os
+import sys
+import requests
+import toml
+import time
+import json
+from logging import getLogger, getLevelName, Formatter, StreamHandler
+
+logger = getLogger(__name__)
+logger.setLevel(getLevelName('DEBUG'))
+log_formatter = \
+    Formatter("%(asctime)s [%(levelname)s] %(name)s: "
+              "%(message)s")
+console_handler = StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger.addHandler(console_handler)
+
+
+class GitlabRunnerAPI:
+    def __init__(self, remote_api, runner_registration_token):
+        self.gl = remote_api
+        self.registration_token = runner_registration_token
+
+    def register(self, name, tags):
+        """Register a new runner with the given name and tag list"""
+        registration_config = {
+            'token': self.registration_token,
+            'description': name,
+            'tag_list': tags,
+            'run_untagged': 'false',
+        }
+        return self.gl.runners.create(registration_config)
+
+    def runners(self):  # pragma: nocover
+        return self.gl.runners.list()
+
+    def unregister(self, runner):  # pragma: nocover
+        """Remove the given runner from the server."""
+        self.gl.runners.delete(runner.id)
+
+    def find_by_name(self, name):  # pragma: nocover
+        """Find a runner with a description matching _name_. Return
+        the matching runner structure"""
+        for runner in self.runners():
+            if runner.description == name:
+                return runner
+
+    def tags(self, runner_id):  # pragma: nocover
+        runner_details = self.gl.runners.get(runner_id)
+        return runner_details.tag_list
+
+    def active_jobs(self, runner_id):  # pragma: nocover
+        runner_details = self.gl.runners.get(runner_id)
+        return runner_details.jobs.list(status='running')
+
+    def set_tags(self, runner_id, tags):  # pragma: nocover
+        runner = self.gl.runners.get(runner_id)
+        runner.tag_list = tags
+        runner.save()
+
+
+class GitlabConfig:
+    DEFAULT_CONFIG = {
+        'check_interval': 0,
+        'concurrent': 128,
+        'session_server': {'session_timeout': 1800}
+    }
+
+    def _save(self):
+        logger.debug(f"saving to {self.config_path} {GitlabConfig.DEFAULT_CONFIG}")
+        with open(self.config_path, 'w') as f:
+            toml.dump(self.config, f)
+
+    def _reload_config(self):
+        # Note: It's quite inefficient to reload the configuration on
+        # our property accesses, but it keeps the client usage very
+        # simple, and this isn't a high throughput situation, so screw
+        # it.
+        try:
+            self.config = toml.load(self.config_path) or \
+                GitlabConfig.DEFAULT_CONFIG
+        except FileNotFoundError:
+            self.config = GitlabConfig.DEFAULT_CONFIG
+
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self._reload_config()
+        self._save()
+        logger.debug("starting with the following configuration:\n"
+                     f"{pformat(self.config)}")
+
+    def runners(self):
+        self._reload_config()
+        if "runners" not in self.config:
+            self.config["runners"] = []
+        return self.config["runners"]
+
+    def find_by_name(self, name):
+        for runner in self.runners():
+            if runner["name"] == name:
+                return runner
+
+    def remove_runner(self, runner):
+        self.runners()[:] = [r for r in self.runners()
+                             if r['name'] != runner['name']]
+        self._save()
+
+    def add_runner(self, name, token):
+        volumes = [
+            '/var/run/docker.sock:/var/run/docker.sock',
+            '/cache'
+        ]
+        config = {
+            'name': name,
+            'limit': 1,
+            'url': 'https://gitlab.freedesktop.org/',
+            'token': token,
+            'executor': 'docker',
+            'custom_build_dir': {},
+            'cache': {'s3': {}, 'gcs': {}, 'azure': {}},
+            'docker': {
+                'tls_verify': False,
+                'image':
+                'registry.freedesktop.org/mupuf/valve-infra/gitlab-job-runner',
+                'privileged': True,
+                'disable_entrypoint_overwrite': False,
+                'oom_kill_disable': False,
+                'disable_cache': False,
+                'volumes': volumes,
+                'network_mode': 'host',
+                'shm_size': 0
+            }
+        }
+        logger.info(f"adding a new runner:\n{pformat(config)}")
+        self.runners().append(config)
+        logger.debug(f"new configuration:\n{pformat(self.config)}")
+        self._save()
+
+
+def sync_tags(runner_api, name, local_tags):  # pragma: nocover
+    remote_runner = runner_api.find_by_name(name)
+    remote_tags = runner_api.tags(remote_runner.id)
+    if not set(local_tags) == set(remote_tags):
+        logger.info(f"""
+local vs remote tags are out of sync: {local_tags} vs
+{remote_tags} setting remote tags to local tags...
+""")
+        runner_api.set_tags(remote_runner.id, local_tags)
+
+
+def sync_mars_machine_with_coordinator(machine, gitlab_config, runner_api):
+    name, tags = machine["full_name"], machine["tags"]
+
+    local_runner = gitlab_config.find_by_name(name)
+    remote_runner = runner_api.find_by_name(name)
+
+    def register():
+        resp = runner_api.register(name, tags)
+        gitlab_config.add_runner(name, resp.token)
+
+    if not local_runner and not remote_runner:
+        logger.info(f"There is neither a local nor remote runner for {name}."
+                    "Registering...")
+        register()
+    elif not local_runner and remote_runner:
+        logger.info(f"There is remote runner named {name}, but not local."
+                    "Deleting remote runner and reregistering...")
+        running_jobs = runner_api.active_jobs(remote_runner.id)
+        if running_jobs:
+            logger.error("The remote runner is actively running jobs. "
+                         "For now this means you should manually sort "
+                         "that out and come back.")
+            return False
+        runner_api.unregister(remote_runner)
+        register()
+    elif local_runner and not remote_runner:
+        logger.info(f"{name} exists locally but not on the remote. "
+                    "Removing locally and reregistering...")
+        gitlab_config.remove_runner(local_runner)
+        register()
+    elif local_runner and remote_runner:
+        pass
+
+    # Both sides are in agreement now, make sure the tags are in agreement too!
+    sync_tags(runner_api, name, tags)
+
+    return True
+
+
+def mars_fetch_machines(mars_host, gitlab_config, runner_api):  # pragma: nocover
+    logger.info(f"Fetching machines from MaRS at {mars_host}")
+    while True:
+        try:
+            r = requests.get(
+                f'{mars_host}/api/v1/machine/?ready_for_service=true')
+            r.raise_for_status()
+            machines = r.json()
+            if not machines:
+                break
+            logger.info(f"there are {len(machines)} machines ready"
+                        " for service, initializing...")
+            success = True
+            for machine in machines:
+                logger.info(f"configuring\n{pformat(machine)}")
+                if not sync_mars_machine_with_coordinator(machine,
+                                                          gitlab_config,
+                                                          runner_api):
+                    logger.error("An error occurred while synchronizing "
+                                 f"{pformat(machine)}. "
+                                 "Will attempt to refetch machines and "
+                                 "try again")
+                    success = False
+                    break
+            if success:
+                break
+            time.sleep(5)
+        except requests.ConnectionError as err:
+            logger.info(f"Initial machine fetch from MaRS ({mars_host}) "
+                        f"has failed. Error: {err}. "
+                        "Trying again...")
+            time.sleep(5)
+
+
+def parse_iso8601_date(d):
+    # For some reason the datetime parser doesn't like the lone Z, I
+    # thought it was spec'd. It does happily accept +00:00 in lieu of
+    # Z.
+    return datetime.fromisoformat(d.removesuffix("Z"))
+
+
+def relevant_event_diff(diff):
+    if 'values_changed' not in diff:
+        return False
+    values_changed = diff['values_changed']
+    if 'root.ready_for_service' in values_changed and \
+       values_changed['root.ready_for_service']['new_value']:
+        return True
+    return any([field in values_changed for field in
+                ['root.tags', 'root.base_name']])
+
+
+def process_mars_events(events, gitlab_config, runner_api):
+    """Process each event in _events_, if any are relevant for
+GitLab, perform the necessary synchronization.
+
+Returns False if no events were processed, otherwise, it will return
+the time of the last event processed plus 1 microsecond (the smallest
+time unit in datetime) so that clients can do a best-effort refetch of
+events after the last one processed"""
+    if not events:
+        return False
+    logger.info(f"===== events since last fetch =====\n:{pformat(events)}")
+    last_checked = False
+    try:
+        for event in events:
+            if event['category'] != 'machine-updated':
+                logger.debug(f'ignoring {event}, only considering udpate events')
+                last_checked = parse_iso8601_date(event['date'])
+                continue
+
+            diff = json.loads(event['diff'])
+            r = requests.get(event['machine'])
+            r.raise_for_status()
+            machine = r.json()
+            if not relevant_event_diff(diff):
+                logger.info("ignoring event, does not have a relevant diff")
+                last_checked = parse_iso8601_date(event['date'])
+                continue
+            logger.debug(f"processing {event}")
+            if not sync_mars_machine_with_coordinator(machine,
+                                                      gitlab_config,
+                                                      runner_api):
+                logger.error("An error occurred while synchronizing "
+                             f"{pformat(machine)}.")
+                break
+            last_checked = parse_iso8601_date(event['date'])
+        return last_checked
+    except requests.ConnectionError:
+        return last_checked
+
+
+def mars_poller(mars_host, gitlab_config, runner_api):  # pragma: nocover
+    logger.info("Polling for changes in MaRS...")
+    last_checked = datetime.now()
+
+    mars_fetch_machines(mars_host, gitlab_config, runner_api)
+
+    logger.info("Initial machine fetch complete, monitoring for events...")
+    while True:
+        try:
+            r = requests.get(
+                f'{mars_host}/api/v1/events/?since={last_checked.isoformat()}')
+            r.raise_for_status()
+            events = r.json()
+            last_event_time = process_mars_events(events,
+                                                  gitlab_config,
+                                                  runner_api)
+            last_checked = last_event_time or last_checked
+        except requests.ConnectionError:
+            pass
+        finally:
+            time.sleep(5)
+
+
+def initial_sync(config, rapi):  # pragma: nocover
+    """Anything registered remotely that is not known locally, remove it.
+Anything known locally that is not registered remotely, register it.
+This ensure we start in a sane state."""
+    logger.info("Making the coordinator and local runner configuration "
+                "agree...")
+
+    remote_runners = rapi.runners()
+    local_runners = config.runners()
+
+    for runner in remote_runners:
+        logger.debug(f"{runner.description} is registered on the server, "
+                     "tagged with:")
+
+        # Convention used by us
+        name = runner.description
+        local_runner = config.find_by_name(name)
+
+        if not local_runner:
+            logger.warn(f"{runner.description} is registered on the "
+                        "coordinatorbut is not locally, removing from "
+                        "coordinator")
+            rapi.unregister(runner)
+
+    for runner in local_runners:
+        remote_runner = rapi.find_by_name(runner["name"])
+        if not remote_runner:
+            logger.warn(f"{runner['name']} is registered locally but "
+                        "not remotely, registering now")
+            rapi.register(runner)
+
+
+def main():  # pragma: nocover
+    gitlab_config = \
+        GitlabConfig(os.getenv('GITLAB_RUNNER_CONF_FILE',
+                               '/etc/gitlab-runner/config.toml'))
+    access_token = os.getenv('GITLAB_ACCESS_TOKEN', None)
+    registration_token = os.getenv('GITLAB_REGISTRATION_TOKEN', None)
+    if not (access_token and registration_token):
+        logger.error("gitlab tokens unavailable to run the sync service "
+                     "set GITLAB_ACCESS_TOKEN and "
+                     "GITLAB_REGISTRATION_TOKEN to enable this service")
+        sys.exit(1)
+
+    mars_host = os.getenv('MARS_HOST', 'http://10.42.0.1:80')
+
+    remote_api = gitlab.Gitlab(url='https://gitlab.freedesktop.org',
+                               private_token=access_token)
+    runner_api = GitlabRunnerAPI(remote_api, registration_token)
+
+    initial_sync(gitlab_config, runner_api)
+
+    try:
+        mars_poller(mars_host, gitlab_config, runner_api)
+    except KeyboardInterrupt:
+        logger.info("Shutdown, Bye!")
+
+
+if __name__ == '__main__':  # pragma: nocover
+    main()
---
+++ containers/gitlab-sync/test_gitlab_sync.py
@@ -0,0 +1,306 @@
+from datetime import datetime
+import gitlab_sync
+from gitlab_sync import (
+    GitlabRunnerAPI,
+    GitlabConfig,
+    sync_mars_machine_with_coordinator,
+    process_mars_events,
+    parse_iso8601_date,
+    relevant_event_diff,
+)
+import copy
+import tempfile
+import toml
+import random
+import unittest
+from unittest.mock import create_autospec, MagicMock, PropertyMock
+import pytest
+import responses
+
+
+class GitlabRunnerAPITests(unittest.TestCase):
+    def setUp(self):
+        # The Gitlab API is mostly generated dynamically, which makes
+        # testing it a mess. You can't use auto-specing. Lukcily, our
+        # testing needs are minor.
+        self.mock_api = MagicMock()
+        self.mock_api.runners = MagicMock()
+        self.api = GitlabRunnerAPI(self.mock_api,
+                                   'test-registration-token')
+
+    def test_registration(self):
+        self.api.register('name-1', 'tags-1')
+        self.mock_api.runners.create.assert_called_with(
+            {
+                'token': 'test-registration-token',
+                'description': 'name-1',
+                'tag_list': 'tags-1',
+                'run_untagged': 'false'
+            })
+
+
+class MarsSyncTests(unittest.TestCase):
+    def setUp(self):
+        # The Gitlab API is mostly generated dynamically, which makes
+        # testing it a mess. You can't use auto-specing. Lukcily, our
+        # testing needs are minor.
+        self.remote_api = MagicMock()
+        gitlab_sync.sync_tags = MagicMock()
+        self.api = create_autospec(GitlabRunnerAPI, spec_set=True)
+        self.machine_name = 'gfx8-1'
+        self.machine_tags = ['tag1', 'tag2']
+        self.machine = {
+            'full_name': self.machine_name,
+            'tags': self.machine_tags,
+        }
+        self.config = create_autospec(GitlabConfig, spec_set=True)
+
+    def test_noRunners(self):
+        self.api.find_by_name.return_value = None
+        self.config.find_by_name.return_value = None
+        sync_mars_machine_with_coordinator(self.machine,
+                                           self.config,
+                                           self.api)
+        self.api.register.assert_called_with(self.machine_name,
+                                             self.machine_tags)
+
+    def test_localRunnerNoRemote(self):
+        self.api.find_by_name.return_value = None
+        self.config.find_by_name.return_value = 'cookie'
+        type(self.api.register.return_value).token = \
+            PropertyMock(return_value=42)
+        sync_mars_machine_with_coordinator(self.machine,
+                                           self.config,
+                                           self.api)
+        self.config.remove_runner.assert_called_with('cookie')
+        self.api.register.assert_called_with(self.machine_name,
+                                             self.machine_tags)
+        self.config.add_runner.assert_called_with(self.machine_name,
+                                                  42)
+
+    def test_remoteRunnerNoLocalNoActiveJobs(self):
+        pm = PropertyMock(return_value=42)
+        m = MagicMock(return_value=pm)
+        self.api.find_by_name = m
+        self.config.find_by_name.return_value = None
+        self.api.active_jobs.return_value = False
+        type(self.api.register.return_value).token = \
+            PropertyMock(return_value=42)
+        sync_mars_machine_with_coordinator(self.machine,
+                                           self.config,
+                                           self.api)
+        self.api.unregister.assert_called_with(pm)
+        self.api.register.assert_called_with(self.machine_name,
+                                             self.machine_tags)
+        self.config.add_runner.assert_called_with(self.machine_name,
+                                                  42)
+
+    def test_remoteRunnerNoLocalActiveJobs(self):
+        self.api.find_by_name = MagicMock()
+        self.config.find_by_name.return_value = None
+        self.api.active_jobs.return_value = True
+        type(self.api.register.return_value).token = \
+            PropertyMock(return_value=42)
+        sync_mars_machine_with_coordinator(self.machine,
+                                           self.config,
+                                           self.api)
+        self.api.unregister.assert_not_called()
+        self.api.register.assert_not_called()
+        self.config.add_runner.assert_not_called()
+
+    def test_localRunnerAndRemote(self):
+        self.api.find_by_name.return_value = 'cookie1'
+        self.config.find_by_name.return_value = 'cookie2'
+        sync_mars_machine_with_coordinator(self.machine,
+                                           self.config,
+                                           self.api)
+        self.api.unregister.assert_not_called()
+        self.api.register.assert_not_called()
+        self.config.add_runner.assert_not_called()
+
+
+class GitlabConfigTests(unittest.TestCase):
+    def setUp(self):
+        self.tmp_file = tempfile.NamedTemporaryFile()
+
+    def tearDown(self):
+        self.tmp_file.close()
+
+    def load_toml(self):
+        return toml.load(self.tmp_file.name)
+
+    def test_corruptedConfiguration(self):
+        self.tmp_file.write(b'{broke n toml {};;;;}')
+        self.config = GitlabConfig(self.tmp_file.name)
+        assert GitlabConfig.DEFAULT_CONFIG == self.load_toml()
+
+    def test_defaultConfiguration(self):
+        self.config = GitlabConfig(self.tmp_file.name)
+        assert GitlabConfig.DEFAULT_CONFIG == self.load_toml()
+        assert len(self.config.runners()) == 0
+        assert self.config.find_by_name("Johnson") is None
+
+    def test_configurationFileNotFound(self):
+        self.tmp_file.close()
+        self.config = GitlabConfig(self.tmp_file.name)
+        assert GitlabConfig.DEFAULT_CONFIG == self.load_toml()
+        assert len(self.config.runners()) == 0
+        assert self.config.find_by_name("Johnson") is None
+
+    def test_testAddRunner(self):
+        self.config = GitlabConfig(self.tmp_file.name)
+        pop = range(1, 10)
+        for i in pop:
+            self.config.add_runner(f'test-runner-{i}', f'token-{i}')
+            assert len(self.config.runners()) == i
+        for i in random.sample(pop, k=len(pop)):
+            assert self.config.find_by_name('test-runner-x') is None
+            added_runner = self.config.find_by_name(f'test-runner-{i}')
+            assert added_runner is not None
+            assert added_runner['token'] == f'token-{i}'
+            assert added_runner['docker']['image'].find('gitlab-job-runner')
+            assert len(added_runner['docker']['volumes']) == 2
+
+    def test_testRemoveRunner(self):
+        self.config = GitlabConfig(self.tmp_file.name)
+        for i in range(1, 10):
+            self.config.add_runner(f'test-runner-{i}', f'token-{i}')
+        for i in range(1, 5):
+            runner = self.config.find_by_name(f'test-runner-{i}')
+            assert runner is not None
+            self.config.remove_runner(runner)
+            assert len(self.config.runners()) == 10 - i - 1
+
+
+class ProcessMarsEventsTests(unittest.TestCase):
+    def setUp(self):
+        # The Gitlab API is mostly generated dynamically, which makes
+        # testing it a mess. You can't use auto-specing. Lukcily, our
+        # testing needs are minor.
+        self.remote_api = MagicMock()
+        gitlab_sync.sync_mars_machine_with_coordinator = MagicMock()
+        self.api = create_autospec(GitlabRunnerAPI, spec_set=True)
+        self.machine_name = 'gfx8-1'
+        self.machine_tags = ['tag1', 'tag2']
+        self.machine = {
+            'full_name': self.machine_name,
+            'tags': self.machine_tags,
+        }
+        self.config = create_autospec(GitlabConfig, spec_set=True)
+        self.rsps = responses.RequestsMock(False)
+        self.events = [
+            {
+                'category': 'machine-updated',
+                'machine': 'http://10.42.0.1/machines/00:01:02:03:04:05',
+                'diff': '{"values_changed": {"root.ready_for_service": {"new_value": true, "old_value": "false"}}}',
+                'date': '2021-02-22T22:31:31Z'
+            },
+            {
+                'category': 'machine-updated',
+                'machine': 'http://10.42.0.1/machines/11:11:22:33:22:33',
+                'diff': '{"values_changed": {"root.ready_for_service": {"new_value": true, "old_value": "false"}}}',
+                'date': '2021-02-22T22:32:00Z'
+            },
+        ]
+        for event in self.events:
+            # Add an empty machine return value, extend as needed in the future
+            self.rsps.add(responses.GET,
+                          event['machine'],
+                          json={})
+
+        self.rsps.start()
+
+    def tearDown(self):
+        self.rsps.stop()
+        self.rsps.reset()
+
+    def test_noEvents(self):
+        process_mars_events([], self.config, self.api)
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == 0
+
+    def test_basic(self):
+        gitlab_sync.sync_mars_machine_with_coordinator.return_value = True
+        last_checked = process_mars_events(self.events, self.config, self.api)
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == len(self.events)
+        assert last_checked == parse_iso8601_date(self.events[-1]['date'])
+
+    def test_exception(self):
+        events = copy.deepcopy(self.events)
+        events[-1]['date'] = 'not-a-date'
+        gitlab_sync.sync_mars_machine_with_coordinator.return_value = True
+        with pytest.raises(ValueError):
+            process_mars_events(events, self.config, self.api)
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == len(events)
+
+    def test_syncMarsWithCoordinatorFails(self):
+        gitlab_sync.sync_mars_machine_with_coordinator.return_value = False
+        assert process_mars_events(self.events, self.config, self.api) is False
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == 1
+
+    def test_connectionerror(self):
+        events = copy.deepcopy(self.events)
+        events[0]['machine'] = 'http://connection-error.invalid'
+        gitlab_sync.sync_mars_machine_with_coordinator.return_value = True
+        assert process_mars_events(events, self.config, self.api) is False
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == 0
+
+    def test_non_interesting_events(self):
+        events = copy.deepcopy(self.events)
+        for event in events:
+            event['category'] = 'machine-created'
+        assert process_mars_events(events, self.config, self.api) == \
+            parse_iso8601_date(events[-1]['date'])
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == 0
+
+    def test_irrelevant_events(self):
+        events = copy.deepcopy(self.events)
+        for event in events:
+            event['diff'] = '{"values_changed": {"root.ip_address": {"new_value": "1.1.1.1", "old_value": "8.8.8.8"}}}'
+        assert process_mars_events(events, self.config, self.api) == \
+            parse_iso8601_date(events[-1]['date'])
+        assert gitlab_sync.sync_mars_machine_with_coordinator.call_count == 0
+
+
+def test_parse_iso8601_date():
+    assert parse_iso8601_date('2021-02-17T17:04:24.579263Z') == \
+        datetime(2021, 2, 17, 17, 4, 24, 579263)
+
+    assert parse_iso8601_date('2021-02-17T17:04:24.579263') == \
+        datetime(2021, 2, 17, 17, 4, 24, 579263)
+
+    with pytest.raises(ValueError):
+        parse_iso8601_date('garbage.579263')
+
+
+@pytest.mark.parametrize(
+    "diff,expectation",
+    [
+        ({}, False),
+        ({"values_changed": {
+            "root.ready_for_service": {
+                "new_value": True,
+                "old_value": False
+            }
+        }}, True),
+        ({"values_changed": {
+            "root.tags": {
+                "new_value": {'tag1', 'tag2'},
+                "old_value": {'tag1'},
+            }
+        }}, True),
+        ({"values_changed": {
+            "root.base_name": {
+                "new_value": 'martin-farm',
+                "old_value": 'charlie-farm',
+            }
+        }}, True),
+        ({"values_changed": {
+            "root.ip_address": {
+                "new_value": '10.42.0.10',
+                "old_value": '10.42.0.9',
+            }
+        }}, False),
+    ],
+)
+def test_relevant_event_diff(diff, expectation):
+    assert relevant_event_diff(diff) is expectation
