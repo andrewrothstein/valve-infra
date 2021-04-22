9120391e84f31b6307724e4b72386ec63e186c55
Martin Peres <martin.peres@mupuf.org>
2021-04-22 17:31:41 +0300

    gitlab-trigger: update to b2c v0.7
    Let's use an official release of b2c, and use the new line meant to
    know when to turn off the machine:
    
        It's now safe to turn off your computer

--- containers/gitlab-trigger/bin/submit_job.sh
+++ containers/gitlab-trigger/bin/submit_job.sh
@@ -200,7 +200,7 @@ timeouts:
 
 console_patterns:
     session_end:
-        regex: '^\+ poweroff -f\r$'
+        regex: "^.*It's now safe to turn off your computer\r$"
     job_success:
         regex: '^\+ DEQP_EXITCODE=0\r$'
     job_warn:
@@ -214,15 +214,14 @@ deployment:
       url: "http://10.42.0.1:9000/boot/default_kernel"
       cmdline:
         - b2c.container="-ti --tls-verify=false docker://${PULL_THRU_REGISTRY}/mupuf/valve-infra/machine_registration:latest check"
-        - b2c.ntp_peer="10.42.0.1" b2c.pipefail b2c.cache_device=auto
+        - b2c.ntp_peer="10.42.0.1" b2c.pipefail b2c.cache_device=auto  b2c.poweroff_delay=15
         - b2c.container="-v ${CI_JOB_ID}-results:${CI_PROJECT_DIR}/results --tls-verify=false docker://$LOCAL_CONTAINER"
         - b2c.post_container="-v ${CI_JOB_ID}-results:/results -e CI_JOB_ID=${CI_JOB_ID} --tls-verify=false docker://${PULL_THRU_REGISTRY}/mupuf/valve-infra/artifact-reaper:latest"
         - console={{ local_tty_device }},115200 earlyprintk=vga,keep SALAD.machine_id={{ machine_id }}
         - loglevel=6
 
     initramfs:
-      # url: "http://10.42.0.1:9000/boot/default_boot2container.cpio.xz"
-      url: "https://gitlab.freedesktop.org/mupuf/boot2container/-/jobs/8517642/artifacts/raw/releases/HEAD/initramfs.linux_amd64.cpio.xz"
+      url: "http://10.42.0.1:9000/boot/default_boot2container.cpio.xz"
 EOF
 
 set +x
