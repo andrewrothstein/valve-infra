andrewrothstein/valve-infra

commit bf9c2fe53298be50a5deb39d6d4b8a5749c09e38

valvetraces: compile the re pattern for frame uploads only once
Signed-off-by: Andres Gomez <agomez@igalia.com>

valvetraces/src/valvetraces/__init__.py
@@ -332,11 +332,11 @@ class Client:
         if machine_tags is None:
             machine_tags = list(self.machine_tags)
 
+        pattern = re.compile('(?P<frame_id>\d+$)')
         for frame in frames:
             file_name = os.path.basename(frame)
-            pattern = r'(?P<frame_id>\d+$)'
             without_ext, _ = os.path.splitext(frame)
-            m = re.search(pattern, without_ext)
+            m = pattern.search(pattern, without_ext)
             if m is None:
                 print(f"Couldn't identify \"{frame}\" 's frame id. Skipping ...")
                 continue

commit 7f1e706dd23f655b0b5110e5650320f452744695

valvetraces: fix wrong call to re.search
Fixes: bf9c2fe ("valvetraces: compile the re pattern for frame uploads only once")

Signed-off-by: Andres Gomez <agomez@igalia.com>

valvetraces/src/valvetraces/__init__.py
@@ -336,7 +336,7 @@ class Client:
         for frame in frames:
             file_name = os.path.basename(frame)
             without_ext, _ = os.path.splitext(frame)
-            m = pattern.search(pattern, without_ext)
+            m = pattern.search(without_ext)
             if m is None:
                 print(f"Couldn't identify \"{frame}\" 's frame id. "
                       "Skipping ...")