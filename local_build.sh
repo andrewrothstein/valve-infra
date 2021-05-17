562fd8ffbad156f75198a94c38e4df22f9d9ad8e
Charlie Turner <cturner@igalia.com>
2021-05-17 10:40:01 +0100

    local_build.sh: Improve the interface

--- local_build.sh
+++ local_build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-docker build -t registry.freedesktop.org/mupuf/valve-infra/executor -f executor/Dockerfile .
+container=${1:-all}
 
-docker build -t registry.freedesktop.org/mupuf/valve-infra/gitlab-trigger containers/gitlab-trigger
-
-docker build -t registry.freedesktop.org/mupuf/valve-infra/salad salad/
-
-docker build -t registry.freedesktop.org/mupuf/valve-infra/valve-infra .
+case $container in
+    executor|all)
+	echo ">>> Building executor...."
+	docker build -t registry.freedesktop.org/mupuf/valve-infra/executor -f executor/Dockerfile .
+	;;&
+    gitlab-trigger|all)
+	echo ">>> Building gitlab-trigger...."
+	docker build -t registry.freedesktop.org/mupuf/valve-infra/gitlab-trigger containers/gitlab-trigger
+	;;&
+    salad|all)
+	echo ">>> Building salad...."
+	docker build -t registry.freedesktop.org/mupuf/valve-infra/salad salad/
+	;;&
+    valve-infra|all)
+	echo ">>> Building valve-infra...."
+	docker build -t registry.freedesktop.org/mupuf/valve-infra/valve-infra .
+	;;
+esac
