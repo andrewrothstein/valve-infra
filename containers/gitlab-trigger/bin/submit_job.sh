andrewrothstein/valve-infra

commit c5f2d61188100cf75e181b102716da24e2589b76

gitlab-trigger: use hang_detection to reboot the machine if needed
Sometimes, the GPU get hung in a way the kernel cannot recover from. In
this case, the only solution is to reboot. This is what this commit
will do, up to 2 times per job.

containers/gitlab-trigger/bin/submit_job.sh
@@ -158,7 +158,7 @@ eval $(
 
 # Setup the entrypoint based on the job variables
 $BUILDAH config \
-	--cmd '['\"$B2C_TEST_SCRIPT\"']' \
+	--cmd '["/usr/local/bin/hang-detection", '\"$B2C_TEST_SCRIPT\"']' \
 	$test_container || exit 1
 
 if [ -z "$CONTAINER_EXISTS" ]; then
@@ -168,6 +168,16 @@ if [ -z "$CONTAINER_EXISTS" ]; then
     $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get update
     $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get install -y curl
     $BUILDAH_RUN $test_container bash -c "curl $MESA_URL | tar xzf -"
+
+    # Compile the hang-detection tool from parallel-deqp-runner
+    $BUILDAH_RUN $test_container bash -c "curl https://gitlab.freedesktop.org/mesa/parallel-deqp-runner/-/archive/master/parallel-deqp-runner-master.tar.gz | tar xzf -"
+    $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get install -y g++ meson pkg-config libvulkan-dev
+    $BUILDAH_RUN $test_container meson parallel-deqp-runner-master parallel-deqp-runner-master/build
+    $BUILDAH_RUN $test_container ninja -C parallel-deqp-runner-master/build hang-detection
+    $BUILDAH_RUN $test_container cp parallel-deqp-runner-master/build/hang-detection /usr/local/bin/
+    $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get remove -y g++ meson libvulkan-dev
+    $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+    $BUILDAH_RUN $test_container env DEBIAN_FRONTEND=noninteractive apt-get autoclean -y
 fi
 
 $BUILDAH_COMMIT $test_container $LOCAL_CONTAINER || exit 1
@@ -196,6 +206,9 @@ timeouts:
   console_activity:  # Reset every time we receive a message from the logs
     minutes: 2
     retries: 0
+  boot_cycle:
+    hours: 4
+    retries: 2
   overall:           # Maximum time the job can take, not overrideable by the "continue" deployment
     hours: 4
     retries: 0
@@ -206,6 +219,8 @@ console_patterns:
         regex: "^.*It's now safe to turn off your computer\r$"
     job_success:
         regex: $B2C_JOB_SUCCESS_REGEX
+    reboot:
+        regex: "GPU hang detected!"
 
 # Environment to deploy
 deployment:

commit 923c6ed74dc4164ba54f3cf4021c15fc88939424

gitlab-trigger: fix the name of the reboot console pattern
OK, time to start writing tests for this, and add more validation in
the job parsing.

Fixes: c5f2d611881 (gitlab-trigger: use hang_detection to reboot th...)

containers/gitlab-trigger/bin/submit_job.sh
@@ -219,7 +219,7 @@ console_patterns:
         regex: "^.*It's now safe to turn off your computer\r$"
     job_success:
         regex: $B2C_JOB_SUCCESS_REGEX
-    reboot:
+    session_reboot:
         regex: "GPU hang detected!"
 
 # Environment to deploy