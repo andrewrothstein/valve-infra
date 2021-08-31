andrewrothstein/valve-infra

commit f88ddd0e738ac9356a3d4b982ca5c70106240535

executor/client: allow sending files as input for the job's execution
Building on the client's interface change, it is now possible to send
multiple files as part of the job submission process.

This is done by adding 2 parameters (--job-id, and --share-directory),
having the client tarball a user-specified folder, the executor
creating a new bucket and extracting the tarball there, then exposing
it to the job via previously-introduced job template variables.

Finally, the job bucket gets deleted at the end of the execution. More
work on this side will come in further commits.

executor/app.py
@@ -122,12 +122,22 @@ def post_job():
                 return None, 406, f"No machines found matching the tags {wanted_tags}"
 
     class JobRequest:
-        def __init__(self, request, version, raw_job, target, callback_endpoint):
+        def __init__(self, request, version, raw_job, target, callback_endpoint,
+                     job_bucket_initial_state_tarball_file=None, job_id=None):
             self.request = request
             self.version = version
             self.raw_job = raw_job
             self.target = target
             self.callback_endpoint = callback_endpoint
+
+            # Clients may specify a starting state for the job bucket,
+            # this will be a tarball that is extracted prior to the
+            # job starting.
+            self.job_bucket_initial_state_tarball_file = job_bucket_initial_state_tarball_file
+
+            # The executor will ensure job IDs are unique, but use the
+            # client-provided prefix for as a naming convention.
+            self.job_id = job_id
 
             # Callback validation
             if callback_endpoint[0] is None:
@@ -186,6 +196,11 @@ def post_job():
                 raise ValueError("The metadata file has the wrong mimetype: "
                                  "{job_file.mimetype}} instead of application/x-yaml")
 
+            initial_state_tarball_file = request.files.get('job_bucket_initial_state_tarball_file', None)
+            if initial_state_tarball_file and initial_state_tarball_file.mimetype != "application/octet-stream":
+                raise ValueError("The job_bucket_initial_state_tarball file has the wrong mimetype: "
+                                 "{initial_state_tarball_file.mimetype}} instead of application/octet-stream")
+
             # Create a Job object
             raw_job = job_file.read().decode()
             job = Job.from_job(raw_job)
@@ -204,7 +219,9 @@ def post_job():
             endpoint = (remote_addr, callback.get("port"))
 
             super().__init__(request=request, version=1, raw_job=raw_job,
-                             target=job_target, callback_endpoint=endpoint)
+                             target=job_target, callback_endpoint=endpoint,
+                             job_bucket_initial_state_tarball_file=initial_state_tarball_file,
+                             job_id=metadata.get('job_id'))
 
     parsed = JobRequest.parse(flask.request)
 

executor/executor.py
@@ -387,15 +387,18 @@ class JobBucket:
 
     @classmethod
     def from_job_request(cls, minio, request):
-        bucket_name = f"job-{request.job_id}"
+        if request.job_id:
+            bucket_name = f"job-{request.job_id}"
 
-        try:
-            return cls(minio, bucket_name=bucket_name,
-                       initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
-        except ValueError:
-            now = datetime.utcnow().timestamp()
-            return cls(minio, bucket_name=f"{bucket_name}-{now}",
-                       initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
+            try:
+                return cls(minio, bucket_name=bucket_name,
+                           initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
+            except ValueError:
+                now = datetime.utcnow().timestamp()
+                return cls(minio, bucket_name=f"{bucket_name}-{now}",
+                           initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
+        else:
+            return None
 
 
 class Executor(Thread):
@@ -414,6 +417,7 @@ class Executor(Thread):
         self.job_ready = Event()
         self.job_config = None
         self.job_console = None
+        self.job_bucket = None
 
         # Remote artifacts (typically over HTTPS) are stored in our
         # local minio instance which is exposed over HTTP to the
@@ -431,20 +435,27 @@ class Executor(Thread):
         if self.state != MachineState.IDLE:
             raise ValueError(f"The machine isn't idle: Current state is {self.state.name}")
 
+        # Prepare the job bucket
+        self.job_bucket = JobBucket.from_job_request(self.minio, job_request)
+        if self.job_bucket:
+            self.job_bucket.create_owner_credentials("dut",
+                                                     whitelisted_ips=[f'{self.machine.ip_address}/32'])
+
         # Bit nasty to render twice, but better than duplicating
         # template render in the various call-sites within
         # executor. Rendering it up front reduces the chances for
         # mistakes. (Meta-point: using an HTTP query to specify the
         # "target" could avoid this duplication of work, and might
         # actually make more sense)
-        job = Job.render_with_machine(job_request.raw_job, self.machine)
+        job = Job.render_with_resources(job_request.raw_job, self.machine, self.job_bucket)
         logger.debug("renderered job:\n%s", job)
 
         self.state = MachineState.QUEUED
         self.job_request = job_request
         self.job_config = job
-        self.job_console = JobConsole(self.machine.id, job_request.callback_endpoint,
-                                      self.job_config.console_patterns,
+        self.job_console = JobConsole(self.machine.id,
+                                      client_endpoint=job_request.callback_endpoint,
+                                      console_patterns=self.job_config.console_patterns,
                                       client_version=job_request.version)
         self.job_ready.set()
 
@@ -478,9 +489,14 @@ class Executor(Thread):
         logger.info("Caching the kernel...")
         self._cache_remote_artifact("kernel", deploy_strt.kernel_url,
                                     deploy_cnt.kernel_url)
+
         logger.info("Caching the initramfs...")
         self._cache_remote_artifact("initramfs", deploy_strt.initramfs_url,
                                     deploy_cnt.initramfs_url)
+
+        if self.job_bucket:
+            logger.info("Initializing the job bucket with the client's data")
+            self.job_bucket.setup()
 
     def run(self):
         def session_init():
@@ -535,6 +551,8 @@ class Executor(Thread):
                 self.job_console.close()
                 self.job_console = None
                 self.machine.boots.remove_pxelinux_config(self.machine.mac_address)
+                if self.job_bucket:
+                    del self.job_bucket
 
             # Interruptible sleep
             for i in range(cooldown_delay_s):

executor/client.py
@@ -2,10 +2,12 @@
 
 from logging import getLogger, getLevelName, Formatter, StreamHandler
 from dataclasses import dataclass
+from tarfile import TarFile
 
 import traceback
 import argparse
 import requests
+import tempfile
 import termios
 import select
 import socket
@@ -37,7 +39,7 @@ class Response:
 
 class Job:
     def __init__(self, executor_url, job_desc, wait_if_busy=False, callback_host=None,
-                 machine_tags=None, machine_id=None):
+                 machine_tags=None, machine_id=None, job_id=None, share_directory=None):
         self.executor_url = executor_url
         self.job_desc = job_desc
         self.wait_if_busy = wait_if_busy
@@ -45,6 +47,14 @@ class Job:
 
         self.machine_tags = machine_tags
         self.machine_id = machine_id
+        self.job_id = job_id
+        self.share_directory = share_directory
+
+    def _create_archive(self):
+        with (tempfile.NamedTemporaryFile(mode='wb', suffix='tar.gz', delete=False) as fp,
+              TarFile.open(fileobj=fp, mode='w:gz') as tar):
+            tar.add(self.share_directory, arcname="")
+            return fp.name
 
     def _parse_response(self, r):
         try:
@@ -62,6 +72,27 @@ class Job:
             raise ValueError(f"Unsupported response version {version}")
 
     def _setup_connection(self):
+        def queue_job(**kwargs):
+            first_wait = True
+
+            while True:
+                r = requests.post(f"{self.executor_url}/api/v1/jobs", **kwargs)
+                response = self._parse_response(r)
+
+                if r.status_code == 200:
+                    return True, response
+                elif r.status_code == 409 and self.wait_if_busy:
+                    if first_wait:
+                        print("No machines available for the job, waiting: ", end="", flush=True)
+                        first_wait = False
+                    else:
+                        print(".", end="", flush=True)
+                    time.sleep(1)
+                else:
+                    print(f"\nERROR: Could not queue the work: \"{response.error_msg}\"", file=sys.stderr)
+
+                    return False, response
+
         # Set up a TCP server
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
             tcp_server.bind(('', 0))
@@ -71,6 +102,7 @@ class Job:
             # Queue the job
             metadata = {
                 "version": 1,
+                "job_id": self.job_id,
                 "callback": {
                     "port": local_port
                 }
@@ -86,7 +118,76 @@ class Job:
 
             files = [('metadata', ('metadata', json.dumps(metadata), 'application/json')),
                      ('job', ('job', self.job_desc, 'application/x-yaml'))]
+            if self.share_directory:
+                print("Packing up the share_directory")
+                archive_path = self._create_archive()
+                archive_stats = os.stat(archive_path)
+                print("--> Wrote %s bytes..." % archive_stats.st_size)
 
+                with open(archive_path, 'rb') as archive_file:
+                    files.append(('job_bucket_initial_state_tarball_file',
+                                  ('job_bucket_initial_state_tarball_file',
+                                   archive_file,
+                                   'application/octet-stream')))
+
+                    success, response = queue_job(files=files)
+                    if not success:
+                        return None, response
+            else:
+                success, response = queue_job(files=files)
+                if not success:
+                    return None, response
+
+            # We should not have a connection queued, accept it with a timeout
+            print(f"Waiting for the executor to connect to our local port {local_port}")
+            tcp_server.settimeout(5)
+            try:
+                sock, _ = tcp_server.accept()
+            except socket.timeout:
+                raise ValueError("The server failed to initiate a connection")
+
+            # Set the resulting socket's timeout to blocking
+            sock.settimeout(None)
+
+        return sock, response
+
+        # Set up a TCP server
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
+            tcp_server.bind(('', 0))
+            tcp_server.listen(1)
+            local_port = tcp_server.getsockname()[1]
+
+            # Queue the job
+            metadata = {
+                "version": 1,
+                "job_id": self.job_id,
+                "callback": {
+                    "port": local_port
+                }
+            }
+            if self.callback_host is not None:
+                metadata['callback']['host'] = self.callback_host
+
+            if self.machine_id is not None or (self.machine_tags is not None and len(self.machine_tags) > 0):
+                metadata['target'] = {
+                    "id": self.machine_id,
+                    "tags": self.machine_tags
+                }
+
+            files = [('metadata', ('metadata', json.dumps(metadata), 'application/json')),
+                     ('job', ('job', self.job_desc, 'application/x-yaml'))]
+            if self.share_directory:
+                print("Packing up the share_directory")
+                archive_path = self._create_archive()
+                archive_stats = os.stat(archive_path)
+                print("--> Wrote %s bytes..." % archive_stats.st_size)
+
+                # TODO: Delete this file after calling requests
+                archive_file = open(archive_path, 'rb')
+                files.append(('job_bucket_initial_state_tarball_file',
+                              ('job_bucket_initial_state_tarball_file',
+                               archive_file,
+                               'application/octet-stream')))
             first_wait = True
             while True:
                 r = requests.post(f"{self.executor_url}/api/v1/jobs", files=files)
@@ -253,20 +354,32 @@ class Job:
 
     @classmethod
     def from_file(cls, executor_url, path, wait_if_busy=False,
-                  callback_host=None, machine_tags=None, machine_id=None):
+                  callback_host=None, machine_tags=None, machine_id=None,
+                  share_directory=None, job_id=None):
         with open(path) as f:
             job_desc = f.read()
 
         return cls(executor_url, job_desc, wait_if_busy=wait_if_busy,
                    callback_host=callback_host, machine_tags=machine_tags,
-                   machine_id=machine_id)
+                   machine_id=machine_id, share_directory=share_directory,
+                   job_id=job_id)
 
 
 def run_job(args):
-    job = Job.from_file(args.executor_url, args.job_id_prefix, args.job, args.wait,
-                        callback_host=args.callback, machine_tags=args.machine_tag,
-                        machine_id=args.machine_id)
-    status = job.start(args)
+    # Perform validation on the share directory
+    if args.share_directory:
+        if not os.path.isdir(args.share_directory):
+            if os.path.exists(args.share_directory):
+                raise ValueError("The share directory is not a folder")
+            else:
+                os.makedirs(args.share_directory)
+
+    job = Job.from_file(executor_url=args.executor_url, path=args.job,
+                        wait_if_busy=args.wait, job_id=args.job_id,
+                        callback_host=args.callback, machine_tags=args.machine_tags,
+                        machine_id=args.machine_id, share_directory=args.share_directory)
+
+    status = job.run()
     logger.info("status: %s", status)
     sys.exit(status.status_code)
 
@@ -289,6 +402,10 @@ if __name__ == '__main__':
                             help="Tag of the machine that should be running the job. Overrides the job's target.")
     run_parser.add_argument("-i", "--machine-id",
                             help="ID of the machine that should run the job. Overrides the job's target.")
+    run_parser.add_argument("-s", "--share-directory",
+                            help=("Directory that will be forwarded to the job, and whose changes will be "
+                                  "forwarded back to"))
+    run_parser.add_argument("-j", "--job-id", help="Identifier for the job, if you have one already.")
     run_parser.add_argument("job", help='Job that should be run')
     run_parser.set_defaults(func=run_job)
 

commit d5095210dab9a1e0a125485da09c06adf3b091ec

executor/JobRequest: generate a job id when missing
This should have been done from the beginning, but I guess I overlooked
it...

Fixes: f88ddd0e7 (executor/client: allow sending files as input ...)

executor/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+
 import traceback
 import requests
 import config
@@ -137,6 +139,9 @@ def post_job():
 
             # The executor will ensure job IDs are unique, but use the
             # client-provided prefix for as a naming convention.
+            if job_id is None:
+                now = int(datetime.utcnow().timestamp())
+                job_id = f"untitled-{now}"
             self.job_id = job_id
 
             # Callback validation

executor/executor.py
@@ -18,6 +18,7 @@ import traceback
 import requests
 import tempfile
 import secrets
+import random
 import select
 import shutil
 import socket
@@ -463,18 +464,17 @@ class JobBucket:
 
     @classmethod
     def from_job_request(cls, minio, request):
-        if request.job_id:
-            bucket_name = f"job-{request.job_id}"
-
-            try:
-                return cls(minio, bucket_name=bucket_name,
-                           initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
-            except ValueError:
-                now = datetime.utcnow().timestamp()
-                return cls(minio, bucket_name=f"{bucket_name}-{now}",
-                           initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
-        else:
-            return None
+        # Generate a job id
+        bucket_name = f"job-{request.job_id}"
+        try:
+            return cls(minio, bucket_name=bucket_name,
+                       initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
+        except ValueError:
+            # The bucket already exists, let's try to make it more unique!
+            now = int(datetime.utcnow().timestamp())
+            rand_int = random.randrange(10e6)
+            return cls(minio, bucket_name=f"{bucket_name}-{now}-{rand_int}",
+                       initial_state_tarball_file=request.job_bucket_initial_state_tarball_file)
 
 
 class Executor(Thread):