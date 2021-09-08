737f3523714abdc587bddb37f3e46b2036aebbd8
Charlie Turner <cturner@igalia.com>
2021-09-08 11:32:08 +0100

    submit_job: Quieten the job YAML definition

--- containers/gitlab-trigger/bin/submit_job.sh
+++ containers/gitlab-trigger/bin/submit_job.sh
@@ -6,9 +6,11 @@ __D="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 __ARTIFACTS=10.42.0.1:9000
 
+set +x
 echo "=========== JOB YAML ================="
 cat "$1"
 echo "=========== END OF JOB YAML ==========="
+set -x
 
 python3 "$__D"/client.py run -w "$1"
 __JOB_RESULT=$?
