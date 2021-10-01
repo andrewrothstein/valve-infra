367632e2b79a72d2fcfe69d79629efcf1b00aca4
Charlie Turner <cturner@igalia.com>
2021-10-01 10:55:38 +0100

    submit_job: Slugify CI_JOB_NAME
    Gitlab allows names to contain all sorts of exotic characters,
    ensure they're more pedestrian when using them for executor buckets!

--- containers/gitlab-trigger/bin/submit_job.sh
+++ containers/gitlab-trigger/bin/submit_job.sh
@@ -12,11 +12,17 @@ cat "$1"
 echo "=========== END OF JOB YAML ==========="
 set -x
 
+# https://stackoverflow.com/a/49035906/1291457
+slugify () {
+    echo "$1" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+
 ls -l results
 [ -d results ] && rm -rf results
 mkdir -pv results
 touch results/"${CI_JOB_NAME}".stamp
-python3 "$__D"/client.py run -w "$1" -j "$CI_JOB_NAME" -s results
+__JOB_NAME=$(slugify "$CI_JOB_NAME")
+python3 "$__D"/client.py run -w "$1" -j "$__JOB_NAME" -s results
 __JOB_RESULT=$?
 
 exit $__JOB_RESULT
