468711e66a89dc2af0d1d48946c7736609f5d0a5
Clayton Craft <clayton@craftyguy.net>
2022-01-04 17:04:54 -0800

    tools/vivian-tmux: fix window naming
    This was failing for me (on tmux 3.2a), because it wasn't able to find a
    window :0 after creating the setting. I'm not sure if that's necessary
    since you can set an initial window name when creating the session,
    which is what I've done here. The window numbering starts at 1.

--- tools/vivian-tmux.sh
+++ tools/vivian-tmux.sh
@@ -7,26 +7,25 @@ D="$SCRIPT_DIR"/../
 
 __vivian_tmux_session="Vivian"
 if !  tmux list-sessions | grep -q $__vivian_tmux_session ; then
-    tmux new-session -d -s $__vivian_tmux_session
+    tmux new-session -d -s $__vivian_tmux_session -n 'Status'
 
-    tmux rename-window -t 0 'Status'
     #tmux send-keys -t 'Status' "make -C $D/containers/valve-infra/ REGISTRY=10.0.2.2:8088 CONTAINER=mupuf/valve-infra/valve-infra-$USER:latest vivian" C-m
     tmux send-keys -t 'Status' "make -C $D/containers/valve-infra/ IPXE_ISO=$D/ipxe-boot-server/infra_isos/ci-gateway-tchar-vivian.iso vivian-ipxe" C-m
 
-    tmux new-window -t $__vivian_tmux_session:1 -n 'Gateway Shell'
+    tmux new-window -t $__vivian_tmux_session:2 -n 'Gateway Shell'
     tmux send-keys -t 'Gateway Shell' C-m 'clear' C-m "make -C $D/containers/valve-infra/ REGISTRY=10.0.2.2:8088 CONTAINER=mupuf/valve-infra/valve-infra-$USER:latest vivian-connect" C-m
 
-    tmux new-window -t $__vivian_tmux_session:2 -n 'vPDU server'
+    tmux new-window -t $__vivian_tmux_session:3 -n 'vPDU server'
     tmux send-keys -t 'vPDU server' "python3 $SCRIPT_DIR/../vivian/vpdu.py --port 9191" C-m
 
-    tmux new-window -t $__vivian_tmux_session:3 -n 'vPDU status'
+    tmux new-window -t $__vivian_tmux_session:4 -n 'vPDU status'
     tmux send-keys -t 'vPDU status' "watch -n10 python3 $SCRIPT_DIR/../vivian/client.py --status" C-m
 
-    tmux new-window -t $__vivian_tmux_session:4 -n 'Gateway PDUs'
+    tmux new-window -t $__vivian_tmux_session:5 -n 'Gateway PDUs'
     tmux send-keys -t 'Gateway PDUs' "watch -n10 \"curl -sL localhost:8001/api/v1/pdu | jq\"" C-m
 
-    tmux new-window -t $__vivian_tmux_session:5 -n 'Gateway Machines'
+    tmux new-window -t $__vivian_tmux_session:6 -n 'Gateway Machines'
     tmux send-keys -t 'Gateway Machines' "watch -n10 \"curl -sL localhost:8000/api/v1/machines | jq\"" C-m
 fi
 
-tmux attach-session -t $__vivian_tmux_session:0
+tmux attach-session -t $__vivian_tmux_session
