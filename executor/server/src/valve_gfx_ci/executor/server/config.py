9d10f258cae6cf3ed1d2be2ab11d334ad38ec484
Charlie Turner <cturner@igalia.com>
2022-01-26 12:58:13 +0000

    config: Look up static templates relative to module.
    Next issue using the virtual testing was,
    
        Traceback (most recent call last):
          File "/usr/local/lib/python3.9/site-packages/valve_gfx_ci/executor/server/mars.py", line 404, in run
            self.sync_machines()
          File "/usr/local/lib/python3.9/site-packages/valve_gfx_ci/executor/server/mars.py", line 360, in sync_machines
            gitlab.generate_runner_config(self.mars_db)
          File "/usr/local/lib/python3.9/site-packages/valve_gfx_ci/executor/server/gitlab.py", line 44, in generate_runner_config
            with open(config.GITLAB_CONF_TEMPLATE_FILE) as f:
        FileNotFoundError: [Errno 2] No such file or directory:
        '/app/templates/gitlab_runner_config.toml.j2'
    
    We no longer install the templates in the /app directory. We have two
    options,
    
     1. Add a helper to look up the templates relative to the config
     module
     2. Symlink the template directories back into /app, as before.
    
    2. feels more like a hack to me, so 1. has been chosen to fix this issue.
    
    Also remove the environment hardcodings to /app/job_templates, since
    they no longer exist!

--- executor/server/src/valve_gfx_ci/executor/server/config.py
+++ executor/server/src/valve_gfx_ci/executor/server/config.py
@@ -1,17 +1,28 @@
 import os
 from typing import Dict
 
+BASE_DIR = os.path.dirname(__file__)
+
+
+def template(filename):
+    return os.path.join(os.path.join(BASE_DIR, 'templates'), filename)
+
+
+def job_template(filename):
+    return os.path.join(os.path.join(BASE_DIR, 'job_templates'), filename)
+
+
 configurables = {
     'EXECUTOR_HOST': '0.0.0.0',
     'EXECUTOR_PORT': 80,
-    'EXECUTOR_REGISTRATION_JOB': '/app/job_templates/register.yml.j2',
-    'EXECUTOR_BOOTLOOP_JOB': '/app/job_templates/bootloop.yml.j2',
+    'EXECUTOR_REGISTRATION_JOB': job_template('register.yml.j2'),
+    'EXECUTOR_BOOTLOOP_JOB': job_template('bootloop.yml.j2'),
     'SERGENT_HARTMAN_BOOT_COUNT': '100',
     'SERGENT_HARTMAN_QUALIFYING_BOOT_COUNT': '100',
     'SERGENT_HARTMAN_REGISTRATION_RETRIAL_DELAY': '120',
     'GITLAB_URL': 'https://gitlab.freedesktop.org',
     'GITLAB_CONF_FILE': '/etc/gitlab-runner/config.toml',
-    'GITLAB_CONF_TEMPLATE_FILE': '/app/templates/gitlab_runner_config.toml.j2',
+    'GITLAB_CONF_TEMPLATE_FILE': template('gitlab_runner_config.toml.j2'),
     'FARM_NAME': None,
     'MARS_DB_FILE': '/app/config/mars.yaml',
     'SALAD_URL': 'http://10.42.0.1:8005',
