0c014872e8c694c67b353e73e6c2124233a58fbe
Charlie Turner <cturner@igalia.com>
2022-02-15 10:56:34 +0000

    ci: Add podman to downstream container
    Quite important to be able to build the downstream containers...

--- .gitlab-ci/mesa-downstream-trigger-container.sh
+++ .gitlab-ci/mesa-downstream-trigger-container.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-pwd ; find . -exec ls -l '{}' \;
-
 python3 -m pip install --upgrade pip
 pip install ./executor/client ./valvetraces
 wget --progres=dot:mega -O /usr/bin/mcli https://dl.min.io/client/mc/release/linux-amd64/mc && chmod +x /usr/bin/mcli
