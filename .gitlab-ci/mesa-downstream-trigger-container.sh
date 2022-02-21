778c3e299d713fc08edd973ff3258a5138dc30b8
Charlie Turner <cturner@igalia.com>
2022-02-21 23:16:19 +0000

    gitlab-trigger: Remove
    This logic is encoded in the Gitlab job YAML now, as per upstream.

--- .gitlab-ci/mesa-downstream-trigger-container.sh
+++ .gitlab-ci/mesa-downstream-trigger-container.sh
@@ -6,10 +6,7 @@ python3 -m pip install --upgrade pip
 pip install ./executor/client ./valvetraces
 wget --progres=dot:mega -O /usr/bin/mcli https://dl.min.io/client/mc/release/linux-amd64/mc && chmod +x /usr/bin/mcli
 
-cp -v containers/gitlab-trigger/bin/ensure_container.sh /usr/local/bin
-cp -v containers/gitlab-trigger/bin/submit_job.sh /usr/local/bin
 pip install ./executor/client ./valvetraces
 
 # Backwards compat.
 ln -sv /usr/local/bin/valvetraces /usr/local/bin/valvetraces.py
-ln -sv /usr/local/bin/executorctl /usr/local/bin/client.py
--- local_build.sh
+++ local_build.sh
@@ -4,14 +4,6 @@ container=${1:-all}
 tag=${2:-latest}
 
 case $container in
-    gitlab-trigger|all)
-	echo ">>> Building gitlab-trigger...."
-	podman build -t registry.freedesktop.org/mupuf/valve-infra/gitlab-trigger -f containers/gitlab-trigger/Dockerfile .
-	;;&
-    salad|all)
-	echo ">>> Building salad...."
-	podman build -t registry.freedesktop.org/mupuf/valve-infra/salad salad/
-	;;&
     machine_registration|all)
 	echo ">>> Building machine_registration...."
 	if [ -z "$IMAGE_NAME" ]; then
@@ -20,8 +12,4 @@ case $container in
 	fi
 	buildah unshare -- sh .gitlab-ci/machine-registration-container-build.sh
 	;;&
-    valve-infra|all)
-	echo ">>> Building valve-infra...."
-	podman build -t registry.freedesktop.org/mupuf/valve-infra -f containers/valve-infra/Dockerfile containers/valve-infra
-	;;
 esac
