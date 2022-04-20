b5bfe8a9c4af97981a64bf03b558da2b7136d644
Clayton Craft <clayton@craftyguy.net>
2022-04-20 13:46:52 -0700

    ansible/dashboard/services: allow specifying expected status for service
    This allows passing a "name:expected status" to the script, and the
    color is selected based on whether the current status matches the
    expected status.
    
    This is useful for services that might not be "active" when things are
    all OK, e.g. if the service is a oneshot thing like nftables.service. We
    don't want to alarm folks by having that be red or yellow, but still
    want to make it very clear when the status is not expected (e.g.
    "failed")
    
    If an expected status isn't specified, it defaults to "active". The
    color for "activating" is still yellow, since I don't think any service
    would be expected to be stuck in "activating" and I think it's useful to
    show that status as visually different than the others.

--- ansible/roles/dashboard/files/bin/services.sh
+++ ansible/roles/dashboard/files/bin/services.sh
@@ -6,22 +6,26 @@ set -eu
 
 [ -z "$@" ] && echo "${red}service name required!${normal}" && exit 1
 
-# $1: service to print status for
+# $1: service to print status for, in the format: name[:expected_status]
+# expected_status is optional, defaulting to "active" if not specified.
 show_status() {
 	local __service="$1"
 	local __status="$(systemctl is-active "$__service")"
 	local __color
-	case "$__status" in
-		"active")
-			__color=$green
-			;;
-		"inactive")
-			__color=$red
-			;;
-		*)
-			__color=$yellow
-			;;
-	esac
+	local __expected="active"
+
+	if echo "$1" | grep -q ":"; then
+		__service="$(echo "$1" | cut -d":" -f1)"
+		__expected="$(echo "$1" | cut -d":" -f2)"
+	fi
+
+	if [ "$__status" = "activating" ]; then
+		__color=$yellow
+	elif [ "$__status" = "$__expected" ]; then
+		__color=$green
+	else
+		__color=$red
+	fi
 
 	printf "%-18s %s\n" "$__service" "${__color}$__status${normal}"
 }
